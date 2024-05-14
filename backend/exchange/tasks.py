54231c0dfedf5eeefb0eca3600fa3b63b9d13d19
Łukasz Ryndak <235333@student.uek.krakow.pl>
2024-05-14 16:49:24 +0200

    Add About site

--- backend/exchange/tasks.py
+++ backend/exchange/tasks.py
@@ -4,6 +4,7 @@ from exchange.email import Email, Plot
 from exchange.api import NBP
 from kernel.celery import app
 from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 import pytz
 
 @app.task
@@ -35,6 +36,37 @@ def checkData():
             end_date: datetime
             end_date = datetime.today().date()
             start_date = end_date- timedelta(days=30)
+            
+            delta = timedelta(days=1)
+            print(start_date,end_date)
+            while start_date <= end_date:
+                data = getDataByDate(start_date,i[0])
+                if data.count()<1:
+                    try:
+                        exchange = api.get(i[0], start_date)
+                        exchange.save()
+                    except Exception as ex:
+                        print("Exception: ", ex)
+                    
+                start_date += delta
+        except Exception as ex:
+            print("Exception: ", ex)
+    exchange = getDataByTypeName(EXCHANGE_NAME[0][0], 1)
+    if(exchange.count()<1):
+        try:
+            exchange = Exchange(name=EXCHANGE_NAME[0][0], midValue=1, bidValue=1, askValue=1, date=datetime.today().date())
+            exchange.save()
+        except Exception as ex:
+            print("Exception: ", ex)
+
+@app.task
+def check5YearsData():
+    api = NBP()
+    for i in EXCHANGE_NAME[1:]:
+        try:
+            end_date: datetime
+            end_date = datetime.today().date()
+            start_date = end_date- relativedelta(years=5)
             
             delta = timedelta(days=1)
             print(start_date,end_date)
--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
-    <h1>About ExchangeRate</h1>
+        <title>About ExchangeRate</title>
+        <p>The application is written in Python Django and Vue.js for currency exchange. On the main screen, all currencies are compared to gold and charts are displayed. Additional "buy" and "sell" buttons redirect to a new view for entering the amount. All data is stored in a PostgreSQL database.</p>
+        <h2>Currency exchange application functionalities:</br> Basic:</h2>
+        <ul>
+          <li>Currency exchange: The ability to exchange different currency pairs at current rates.</li>
+          <li>Currency calculator: Calculating amounts in different currencies.</li>
+          <li>Transaction history: Reviewing the history of your currency exchanges.</li>
+          <li>Exchange rates: Access to live current exchange rates.</li>
+          <li>Exchange rate charts: Tracking changes in exchange rates over time.</li>
+        </ul>
+        <h2>Additional:</h2>
+        <ul>
+          <li>Exchange rate alerts: Setting email alerts about exchange rate changes.</li>
+          <li>Currency wallets: Storing different currencies in one place.</li>
+          <li>Currency information: Access to information about different currencies.</li>
+        </ul>
   </div>
 </template>
